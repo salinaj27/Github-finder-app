7e73397261d607479ec0de027eca72bbe78a4484
salinaj27 <salinajohn80@gmail.com>
2022-05-10 08:54:58 -0400

    created repo component and added latest repos in main user

---
+++ craco.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  style: {
+    poatcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+};
---
+++ src/components/layout/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../../users/UserResults";
+import UserSearch from "../../users/UserSearch";
+
+function Home() {
+  return (
+    <>
+      <UserResults />
+      <UserSearch />
+    </>
+  );
+}
+
+export default Home;
