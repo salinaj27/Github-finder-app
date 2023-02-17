59b14b6692f15bc54151addc40dbfebb3ffc8958
salinaj27 <salinajohn80@gmail.com>
2023-02-17 10:01:47 -0500

    testing

--- src/components/layout/pages/About.jsx
+++ src/components/layout/pages/About.jsx
@@ -18,12 +18,7 @@ function About() {
       <p className="text-lg text-gray-400">
         Version <span className="text-white">1.0.0</span>
       </p>
-      <p className="text-lg text-gray-400">
-        Layout By:
-        <a className="text-white" href="https://twitter.com/hassibmoddasser">
-          Hassib Moddasser
-        </a>
-      </p>
+      <p className="text-lg text-gray-400"></p>
     </div>
   );
 }
